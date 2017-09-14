ca315ad46d450b52a3b2cd765febc74afb32bfe4
Angular CLI <angular-cli@angular.io>
2017-09-13 20:19:04 -0700

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SabWeddingSitePage } from './app.po';
+
+describe('sab-wedding-site App', () => {
+  let page: SabWeddingSitePage;
+
+  beforeEach(() => {
+    page = new SabWeddingSitePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
